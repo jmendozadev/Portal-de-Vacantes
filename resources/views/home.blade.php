5dfe17050db6152792e235d2d01b91f1ee414869
Jesús Mendoza <jesusdlc.m15@gmail.com>
2024-12-05 18:07:48 -0600

    añadir nueva vista de la homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,88 +6,27 @@
 
 <!-- Estilos personalizados -->
 @push('styles')
-    <style>
-        html {
-            background-color: #fff;
-            box-sizing: border-box; /* Hack para Box Model */
-        }
-        *:before,
-        *:after {
-            box-sizing: inherit;
-        }
-        body {
-            font-size: 16px; /* 1 rem = 10px */
-            font-family: 'Krub', sans-serif;
-        }
-        .search-bar input, .search-bar select, .search-bar button {
-            border-radius: 50px;
-            padding: 10px 20px;
-        }
-        .links a {
-            text-decoration: none;
-        }
-        .hero-section {
-            background: linear-gradient(rgba(0, 0, 0, 0.694), rgba(0, 0, 0, 0.671)), url('../images/bg-home.png') no-repeat center center;
-            background-size: cover;
-            color: white;
-            padding: 225px 0px;
-            text-align: center;
-        }
-        .footer {
-            background: #fff;
-            padding: 30px 0;
-            text-align: center;
-        }
-        .logo-coatza {
-            margin-bottom: 40px;
-        }
-        .social-icons a {
-            /* separa los iconos con margin */
-            margin: 0 10px;
-        }
-
-        /* Media Queries: Para mackbook 14 pulgadas */
-        @media (max-width: 1512px) {
-            .search-bar input, .search-bar select, .search-bar button {
-            border-radius: 50px;
-            padding: 10px 20px;
-            }
-            .links a {
-                text-decoration: none;
-            }
-            .hero-section {
-                background: linear-gradient(rgba(0, 0, 0, 0.694), rgba(0, 0, 0, 0.671)), url('../images/bg-home.png') no-repeat center center;
-                background-size: cover;
-                color: white;
-                padding: 120px 0;
-                text-align: center;
-            }
-            .footer {
-                background: #fff;
-                padding: 30px 0;
-                text-align: center;
-            }
-            .logo-coatza {
-                margin-bottom: 40px;
-            }
-            .social-icons a {
-                /* separa los iconos con margin */
-                margin: 0 10px;
-            }
-        }
-    </style>
+    <link rel="preload" href="{{ asset('css/style.css') }} as="style">
+    <link rel="stylesheet" href="{{ asset('css/homepage-style.css') }}">
 @endpush
 
 @section('header')
     <!-- Header -->
-        <div class="container d-flex justify-content-between align-items-center">
-            <img src="images\logo.png" alt="Logo" height="60">
-            <nav class="links">
+        <div class="container header d-flex justify-content-between align-items-center">
+            <div class="logo-header">
+                <img src="images\logo.png" alt="Logo" height="60">
+            </div>
+            <nav class="links d-flex flex-wrap justify-content-center">
                 <a href="#" class="text-white mx-2">INICIO</a>
                 <a href="#" class="text-white mx-2">LISTA DE EMPLEOS</a>
                 <a href="#" class="text-white mx-2">EMPRESAS</a>
                 <a href="#" class="text-white mx-2">CONTACTO</a>
-                <a href="#" class="btn btn-danger text-white mx-2">REGÍSTRATE</a>
+                <div>
+                    <a href="#" class="btn bg-white text-white mx-1">
+                        <img src="images\login-icon.png" alt="Login" width="20">
+                    </a>
+                    <a href="#" class="btn btn-danger text-white mx-1">REGÍSTRATE</a>
+                </div>
             </nav>
         </div>
 @endsection
@@ -96,13 +35,15 @@
     @section('content')
         <section class="hero-section">
             <div class="container">
-                <h1>Bolsa De Trabajo Emplé@te</h1>
-                <p>H. Ayuntamiento de Coatzacoalcos, Ver.</p>
+                <div class="hero-caption">
+                    <h1>Bolsa De Trabajo Emplé@te</h1>
+                    <p>H. Ayuntamiento de Coatzacoalcos, Ver.</p>
+                </div>
                 <div class="search-bar d-flex justify-content-center mt-4">
-                    <select class="form-select me-2" style="max-width: 300px;">
+                    <select class="form-select me-2" style="max-width: 400px;">
                         <option selected>Seleccionar Categoría</option>
                     </select>
-                    <select class="form-select me-2" style="max-width: 300px;">
+                    <select class="form-select me-2" style="max-width: 400px;">
                         <option selected>Seleccionar Municipio</option>
                     </select>
                     <button class="btn btn-warning">Buscar</button>
@@ -115,7 +56,7 @@
 @section('footer')
         <div class="container">
             <img src="/images/coatza-logo.png" alt="Coatzacoalcos" height="100" class="logo-coatza">
-            <div class="social-icons mt-3">
+            <div class="social-icons mt-5">
                 <a href="#"><img src="/images/facebook-icon.png" alt="Facebook" width="40"></a>
                 <a href="#"><img src="/images/tiktok-icon.png" alt="TikTok" width="40"></a>
                 <a href="#"><img src="/images/x-icon.png" alt="Twitter" width="40"></a>
--- resources/views/layouts/index-layout.blade.php
+++ resources/views/layouts/index-layout.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="icon" href="images/window-logo.png">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub&display=swap" rel="stylesheet">
     <!-- Estilos personalizados -->
     @stack('styles')
     
